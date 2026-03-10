2d3f24f1a891fd52b9079f84efad197582fcbf78
Cristian Vultur <149108004+CristiV225@users.noreply.github.com>
2026-03-10 14:27:19 +0200

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -1,21 +1,30 @@
-INSERT INTO Customers (name, email, phone)
+INSERT INTO Customers (name,email,phone)
 VALUES
 ('Cristian Vultur','vulturcristianmarcel@gmail.com','0712345678'),
 ('Ana Popescu','anap1@gmail.com','0747825943'),
 ('Denisa Marcu','marcud3@gmail.com','0785302573');
 
-INSERT INTO Hotels (hotel_name, city)
+INSERT INTO Hotels (hotel_name,city)
 VALUES
 ('Grand Hotel','Cluj-Napoca'),
 ('City Hotel','Bucharest');
 
-INSERT INTO Rooms (hotel_id, room_number, price_per_night, capacity)
+INSERT INTO RoomTypes (type_name,description)
 VALUES
-(1,101,200,2),
-(1,102,250,3),
-(2,201,300,2);
+('Single','1 person'),
+('Double','2 persons'),
+('Family','4 persons'),
+('Suite','Luxury room');
 
-INSERT INTO Bookings (customer_id, room_id, check_in, check_out)
+INSERT INTO Rooms (hotel_id,type_id,room_number,price_per_night,capacity)
+VALUES
+(1,1,101,200,1),
+(1,2,102,250,2),
+(1,3,103,350,4),
+(2,2,201,300,2),
+(2,4,202,500,2);
+
+INSERT INTO Bookings (customer_id,room_id,check_in,check_out)
 VALUES
 (1,1,'2026-04-01','2026-04-05'),
-(2,3,'2026-04-10','2026-04-12');
+(2,4,'2026-04-10','2026-04-12');
