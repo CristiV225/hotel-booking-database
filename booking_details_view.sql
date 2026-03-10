23fa7d073d2af92fb932ceec2a16f0335cecfe35
Cristian Vultur <149108004+CristiV225@users.noreply.github.com>
2026-03-10 14:27:45 +0200

    Update booking_details_view.sql

--- booking_details_view.sql
+++ booking_details_view.sql
@@ -1,14 +1,16 @@
 CREATE VIEW BookingDetails AS
 SELECT 
-    Customers.name,
-    Hotels.hotel_name,
-    Rooms.room_number,
-    Bookings.check_in,
-    Bookings.check_out
+Customers.name AS Customer,
+Hotels.hotel_name AS Hotel,
+RoomTypes.type_name AS RoomType,
+Rooms.room_number,
+Bookings.check_in,
+Bookings.check_out
 FROM Bookings
-JOIN Customers
-ON Bookings.customer_id = Customers.customer_id
-JOIN Rooms
-ON Bookings.room_id = Rooms.room_id
-JOIN Hotels
-ON Rooms.hotel_id = Hotels.hotel_id;
+JOIN Customers ON Bookings.customer_id = Customers.customer_id
+JOIN Rooms ON Bookings.room_id = Rooms.room_id
+JOIN Hotels ON Rooms.hotel_id = Hotels.hotel_id
+JOIN RoomTypes ON Rooms.type_id = RoomTypes.type_id;
+
+SELECT * FROM BookingDetails;
+
