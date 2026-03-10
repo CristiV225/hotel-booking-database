91c4d6707745d34fa149da93435c7dee7cfa9615
Cristian Vultur <149108004+CristiV225@users.noreply.github.com>
2026-03-10 14:28:49 +0200

    Update create_booking.sql

--- create_booking.sql
+++ create_booking.sql
@@ -1,10 +1,26 @@
 CREATE PROCEDURE CreateBooking
-    @customer_id INT,
-    @room_id INT,
-    @check_in DATE,
-    @check_out DATE
+@customer_id INT,
+@room_id INT,
+@check_in DATE,
+@check_out DATE
 AS
 BEGIN
-    INSERT INTO Bookings (customer_id, room_id, check_in, check_out)
-    VALUES (@customer_id, @room_id, @check_in, @check_out);
+
+IF EXISTS (
+SELECT *
+FROM Bookings
+WHERE room_id=@room_id
+AND @check_in < check_out
+AND @check_out > check_in
+)
+
+PRINT 'Room already booked in this period'
+
+ELSE
+
+INSERT INTO Bookings(customer_id,room_id,check_in,check_out)
+VALUES(@customer_id,@room_id,@check_in,@check_out)
+
 END;
+
+EXEC CreateBooking 1,2,'2026-05-01','2026-05-05';
