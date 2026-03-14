e1d354af22f07ad1f13fd1053e08cddb5dfa0ee4
Cristian Vultur <149108004+CristiV225@users.noreply.github.com>
2026-03-14 21:42:08 +0200

    Update HotelBooking.sql

--- HotelBooking.sql
+++ HotelBooking.sql
@@ -220,8 +220,6 @@ LEFT JOIN Rooms
 ON Hotels.hotel_id = Rooms.hotel_id
 GROUP BY Hotels.hotel_name;
 
--- MOST EXPENSIVE ROOM PER HOTEL
-
 SELECT
 Hotels.hotel_name,
 MAX(Rooms.price_per_night) AS most_expensive_room
